5132fd6b6036c4c8fea0cb52600e93cc18b2e4aa
Marco Aurélio Roque de Oliveira Junior <marcoaureliocraft@gmail.com>
2021-04-06 20:32:17 -0300

    "Finalizado" (alguns bugs)

--- config/script.js
+++ config/script.js
@@ -1,3 +1,46 @@
+cartaEkko = {
+    nome:"Ekko",
+    imagem:"https://th.bing.com/th/id/OIP.EW2dPPKMlAtX8d09IudRYAHaEK?w=315&h=180&c=7&o=5&pid=1.7",
+    atributos: {
+        Attack: 50,
+        Armor: 30,
+        AttackSpeed: 40
+    }
+}
+
+cartaGaren = {
+    nome:"Garen",
+    imagem:"https://th.bing.com/th/id/OIP.O3AmaAiOuZERb5_fYm6NsAHaDt?w=328&h=175&c=7&o=5&pid=1.7",
+    atributos: {
+        Attack: 50,
+        Armor: 70,
+        AttackSpeed: 10
+    }
+}
+
+cartaMasterYi = {
+    nome:"MasterYi",
+    imagem:"https://th.bing.com/th/id/OIP.03eDYm8n1OiekstS1IktzQHaFl?w=234&h=180&c=7&o=5&pid=1.7",
+    atributos: {
+        Attack: 80,
+        Armor: 15,
+        AttackSpeed: 80
+    }
+}
+
+cartaMissFortune = {
+    nome:"MissFortune",
+    imagem:"https://th.bing.com/th/id/OIP.RDUgTJrTv0Dxc6KtU8-LiwHaNL?w=182&h=324&c=7&o=5&pid=1.7",
+    atributos: {
+        Attack: 40,
+        Armor: 20,
+        AttackSpeed: 50
+    }
+
+}
+
+
+
 
 //variaveis
 
@@ -6,29 +49,53 @@ var cartaJogador
 var cartas = [cartaMasterYi, cartaEkko, cartaGaren, cartaMissFortune]
               //0            1          2
 
+//Funçao Atualizar Placar
+var pontosJogador = 0
+var pontosMaquina = 0
+
+atualizaPlacar()
+
+atualizaQuantidadeDeCarta()
+
+function atualizaQuantidadeDeCarta() {
+    var divQuantidadeCartas =  document.getElementById("quantidade-cartas")
+    var html = "Quantidade de cartas no jogo: " + cartas.length
+    
+    divQuantidadeCartas.innerHTML = html
+}
+
+function atualizaPlacar () {
+    var divPlacar = document.getElementById("placar")
+    var html = "Jogador " + pontosJogador + "/" + pontosMaquina + " Maquina"
+
+    divPlacar.innerHTML = html
+}
+
 //Funçao sortear carta 
 
-function sortearCarta(){
-    var numeroCartaMaquina = parseInt(Math.random() * 3)
+function sortearCarta() {
+    var numeroCartaMaquina = parseInt(Math. random() * cartas.length)
     cartaMaquina = cartas[numeroCartaMaquina]
 
-    var numeroCartaJogador = parseInt(Math.random() * 3)
+    cartas.splice(numeroCartaMaquina, 1)
 
-    while(numeroCartaJogador == numeroCartaMaquina){
-        numeroCartaJogador = parseInt(Math.random() * 3)
-
-    } 
+    var numeroCartaJogador = parseInt(Math. random() * cartas.length)
     cartaJogador = cartas[numeroCartaJogador]
-    console.log(cartaJogador)
 
-//Desabilita e abilita botao
+    cartas.splice(numeroCartaJogador, 1)
 
-    document.getElementById('btnSortear').disabled = true
-    document.getElementById('btnJogar').disabled = false
+   
+
+    
+    
+    //Desabilita e abilita botao
+
+    document. getElementById('btnSortear'). disabled = true
+    document. getElementById('btnJogar'). disabled = false
 
     exibeCartaJogador()
-
 }
+
 
 function exibeCartaJogador(){
     var divCartaJogador = document.getElementById("carta-jogador")
@@ -69,15 +136,38 @@ function jogar(){
     
    if(cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]){
     htmlResultado = `<p class="resultado-final"> Você ganhou, parabens </p>`
+
+    pontosJogador++
+
    }else if(cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]){
     htmlResultado = `<p class="resultado-final"> Você perdeu, mais sorte na proxima. </p>`
+
+    pontosMaquina++
+    
    }else{
     htmlResultado = `<p class="resultado-final"> Empatou, tente novamente </p>`
    }
 
+   if(cartas.length == 0) {
+       alert("Fim de jogo")
+       if(pontosMaquina > pontosMaquina ) {
+           htmlResultado = '<p class="resultado-final>Parabens você ganhou!</p>" '
+       }else if(pontosMaquina > pontosJogador) {
+           htmlResultado = '<p class="resultado-final>Infelizmente você perdeu, mais sorte na proxima.</p>" '
+       }else{
+           htmlResultado = '<p class="resultado-final>Empatou!</p>" '
+       }
+   }else{
+       document.getElementById("btnProximaRodada").disabled = false
+   }
+
    divResultado.innerHTML = htmlResultado
 
+   document.getElementById("btnJogar").disabled = true
+
+   atualizaPlacar()
    exibeCartaMaquina()
+   atualizaQuantidadeDeCarta()
 }
 
 function exibeCartaMaquina(){
@@ -96,4 +186,18 @@ function exibeCartaMaquina(){
     var html = "<div id='opcoes' class='carta-status'>"
 
     divCartaMaquina.innerHTML = moldura+nome+html+opcoesTexto+'</div>'
+}
+
+function proximaRodada() {
+    var divCartas = document.getElementById("cartas")
+
+    divCartas.innerHTML = `<div id="carta-jogador" class="carta"></div> <div id="carta-maquina" class="carta"></div>`
+
+    document.getElementById("btnSortear").disabled = false
+    document.getElementById("btnJogar").disabled = true
+    document.getElementById("proximaRodada").disabled = true
+
+    var divResultado = document.getElementById("resultado")
+
+    divResultado.innerHTML = ""
 }
